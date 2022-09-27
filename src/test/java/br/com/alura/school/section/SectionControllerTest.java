1d5930f5ae21597000c42e4a59d1492b80c04777
Lucas Guilha <medowlucas@gmail.com>
2022-09-27 17:46:57 -0300

    implementação do noContent test na SectionControllerTest

--- src/test/java/br/com/alura/school/section/SectionControllerTest.java
+++ src/test/java/br/com/alura/school/section/SectionControllerTest.java
@@ -42,39 +42,14 @@ public class SectionControllerTest {
     @Autowired
     private VideoRepository videoRepository;
 
-    // @Test
-    // void should_add_new_enroll() throws Exception {
-    //     final String courseCodeSample = "java-3";
-    //     final String usernameSample = "janaina";
-
-    //     userRepository.save(new User(usernameSample, usernameSample + "@email.com"));
-    //     courseRepository.save(new Course(courseCodeSample, "Course of " + courseCodeSample, "Java and Object Orientation: Encapsulation, Inheritance and Polymorphism."));
-    //     NewEnrollRequest newEnroll = new NewEnrollRequest(usernameSample);
-
-    //     mockMvc.perform(post("/courses/{courseCode}/enroll", courseCodeSample)
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(jsonMapper.writeValueAsString(newEnroll)))
-    //             .andDo(print())
-    //     .andExpect(status().isCreated());
-    // }
-
-    // @ParameterizedTest
-    // @CsvSource({
-    //         "'',",
-    //         "     ,",
-    //         "an-username-that-is-really-really-big"
-    // })
-    // void should_validate_bad_enroll_requests(String username) throws Exception {
-    //     NewEnrollRequest newEnroll = new NewEnrollRequest(username);
-
-    //     mockMvc.perform(post("/courses/java-1/enroll")
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(jsonMapper.writeValueAsString(newEnroll)))
-    //             .andExpect(status().isBadRequest());
-    // }
+    @Test
+    void should_display_no_content_section_report_videos() throws Exception {
+        mockMvc.perform(get("/sectionByVideosReport"))
+                .andExpect(status().isNoContent());
+    }
 
     @Test
-    void should_not_show_duplicated_sections_response_when_has_two_enrolls() throws Exception {
+    void should_not_show_duplicated_section_report_video_response_when_has_two_enrolls() throws Exception {
 
         final String sectionTitle = "Aula de testes";
         final String usernameSample1 = "adriana";
